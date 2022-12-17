3ca671089e217581bce1f36dcaad68ae181aae9f
Abi Matthews-Gordon <60277772+AbiMatthews-Gordon@users.noreply.github.com>
2022-12-17 02:00:48 -0500

    contact complete

---
+++ cypress/e2e/data/contact.data.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    contacts: {
+        subject: 'Bad Product',
+        message: 'I purchased a hat and it was not the colour i ordered.',
+        confirmationMessage: 'Message Sent!Your message has been sent',
+        errorMessage: 'Field is required!'
+    }
+
+}
